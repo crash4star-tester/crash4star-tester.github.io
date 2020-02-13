dddb438dddcf3f39324364e0782fbe33b8f1841f
crash4star-tester <60957790+crash4star-tester@users.noreply.github.com>
2020-02-13 15:37:38 +0300

    more

---
+++ fetch.js
@@ -0,0 +1,37 @@
+const requestUrl = 'https://crash4star-tester.github.io/data.json';
+
+function sendRequest(method, url, body = null) {
+    let headers = {
+        'Content-type': 'application/json'
+    };
+
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(body),
+        headers: headers
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return response.json().then(error => {
+                const e = new Error('Something wrong!');
+                e.data = error;
+                throw e;
+            });
+        }
+
+    });
+}
+
+// sendRequest('GET', requestUrl)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+let main = {
+    name: 'Crash4star',
+    age: 24
+}
+
+sendRequest('POST', requestUrl, main)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))

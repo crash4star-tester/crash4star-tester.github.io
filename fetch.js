b21399ddc7d33afb4251f7523dda604f55fef64b
crash4star-tester <60957790+crash4star-tester@users.noreply.github.com>
2020-03-11 12:02:38 +0300

    ASD

--- fetch.js
+++ fetch.js
@@ -1,47 +1,71 @@
+// const requestUrl = 'https://crash4star-tester.github.io/data.json';
+
+// function sendRequest(method, url, body = null) {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+
+//         xhr.open(method, url);
+
+//         xhr.responseType = 'json';
+//         xhr.setRequestHeader('Content-type', 'application/json');
+
+//         xhr.onload = () => {
+//             if(xhr.status >= 400) {
+//                 reject(xhr.response);
+//             } else {
+//                 resolve(xhr.response);
+//             }
+//         }
+
+//         xhr.onerror = () => {
+//             reject(xhr.response);
+//         }
+
+//         xhr.send(JSON.stringify(body));
+//     })
+// }
+
+
+
+
+//Fetch
 const requestUrl = 'https://crash4star-tester.github.io/data.json';
 
 function sendRequest(method, url, body = null) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
+    const headers = {
+        'Content-type': 'application/json'
+    };
 
-        xhr.open(method, url);
-
-        xhr.responseType = 'json';
-        xhr.setRequestHeader('Content-type', 'application/json');
-
-        xhr.onload = () => {
-            if(xhr.status >= 400) {
-                reject(xhr.response);
-            } else {
-                resolve(xhr.response);
-            }
-        }
-
-        xhr.onerror = () => {
-            reject(xhr.response);
-        }
-
-        xhr.send(JSON.stringify(body));
-    })
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(body),
+        headers: headers
+    }).then(response => {
+        return response.json();
+    });
 }
 
 
+
+
 const btn = document.querySelector('#send'),
-      input = document.querySelector('#user');
+    input = document.querySelector('#user');
+
+let userName = {};
 
 
-btn.onclick = function(e) {
+btn.onclick = function (e) {
     e.preventDefault();
 
-    let userName = {};
-
-    userName.name = input.value;
-    input.value = '';
-
-    console.log(userName);
+    userName.nickname = input.value;
+    console.log(userName)
 
     sendRequest('POST', requestUrl, userName)
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
 };
 
+
+// sendRequest('GET', requestUrl, userName)
+//         .then(data => console.log(data))
+//         .catch(err => console.log(err));
